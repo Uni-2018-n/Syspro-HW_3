4119176c6790bc42987c0623bf8502ecf919eadb
smackflad <ask.kalamakhs@hotmail.com>
2021-06-07 01:24:11 +0300

    done with comments fixed some minor bugs

--- monitorServer.cpp
+++ monitorServer.cpp
@@ -9,30 +9,25 @@
 
 using namespace std;
 
-string* pool;
-int sizePool;
+string* pool;//global variables for cyclic buffer
+int cyclicBufferSize;
 int poolStart;
 int poolEnd;
 int currSizePool;
 pthread_mutex_t poolMtx;
 pthread_cond_t poolFull;
 pthread_cond_t poolEmpty;
-pthread_cond_t poolDone;
-pthread_mutex_t currSizeMtx;
 
-GlistHeader* main_list;
-pthread_mutex_t mainListMtx;
+GlistHeader* main_list;//since we need threads to access the main_list we need to make it a global variable
+pthread_mutex_t mainListMtx;//and use a mutex for accessing it
 
-string getCountry(string t){
-    return t.substr(t.find('/')+1);
-}
-
+string getCountry(string t);
 void* threadFunction(void* ptr);
 
 int main(int argc, const char** argv) {
-    int portNum, socketBufferSize, cyclicBufferSize, sizeOfBloom, numThreads;
+    int portNum, socketBufferSize, sizeOfBloom, numThreads;
 
-    if(argc <= 10){//simple steps to set the command line arguments to parameters
+    if(argc <= 10){//simple steps to set the command line arguments to variables
         cout << "Error! Wrong parameters:" << endl;
         cout << "monitorServer -p port -t numThreads -b socketBufferSize -c cyclicBufferSize -s sizeOfBloom path1 path2 ... pathn" << endl;
         return -1;
@@ -53,61 +48,59 @@ int main(int argc, const char** argv) {
         }
     }
 
-    int dirLen = argc - i - 2; //argc - (index of -s) - (first argument is name of executable)
+    int dirLen = argc - i - 2; //argc - (index of sizeOfBloom) - 2 to find how many paths we have
     string pathToDirs[dirLen];
     int j=0;
-    for(i = i+2;i<argc;i++){
+    for(i = i+2;i<argc;i++){//fill pathToDirs array
         pathToDirs[j] = argv[i];
         j++;
     }
 
-    pool = new string[cyclicBufferSize];
+    pool = new string[cyclicBufferSize];//initialize pool buffer
     for(i=0;i<cyclicBufferSize;i++){
         pool[i] = "-1";
     }
-    sizePool = cyclicBufferSize;
-    currSizePool =0;
-    poolStart =0;
+
+    currSizePool = 0;//initialize global virables, mutexes and condition varriables
+    poolStart = 0;
     poolEnd = -1;
     pthread_mutex_init(&poolMtx, 0);
-    pthread_mutex_init(&currSizeMtx, 0);
     pthread_cond_init(&poolEmpty, 0);
-    pthread_cond_init(&poolDone, 0);
     pthread_cond_init(&poolFull, 0);
-
-
-    pthread_t threads[numThreads];
-    for(i=0;i<numThreads;i++){
-        pthread_create(&threads[i], 0, threadFunction, 0);
-    }
 
     main_list = new GlistHeader(sizeOfBloom);
     pthread_mutex_init(&mainListMtx, 0);
 
-    struct hostent *rem;
+    struct hostent *rem;//use localhost since the other end of the socket is in the same machine as this
     if((rem = gethostbyname("localhost")) == NULL){
-        cout << "ERROR" << endl;
+        cout << "monitorServer: gethostbyname ERROR" << endl;
+        exit(-1);
     }
 
     int socke;
     struct sockaddr_in serveradr;
     struct sockaddr* serveradrptr = (struct sockaddr*)&serveradr;
-    if((socke = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-        cout << "error" << endl;
-        exit(1);
+    if((socke = socket(AF_INET, SOCK_STREAM, 0)) < 0){//init socket communication
+        cout << "monitorServer: socket ERROR" << endl;
+        exit(-1);
     }
 
     serveradr.sin_family = AF_INET;
     memcpy(&serveradr.sin_addr, rem->h_addr, rem->h_length);
-    serveradr.sin_port = htons(portNum);
+    serveradr.sin_port = htons(portNum);//setup correct port to use
 
-    if(connect(socke, serveradrptr, sizeof(serveradr)) < 0){
-        cout << "error" << endl;
-        exit(1);
+    if(connect(socke, serveradrptr, sizeof(serveradr)) < 0){//finally send a request for the socket
+        cout << "monitorServer: socket connect ERROR" << endl;
+        exit(-1);
     }
 
-    int countFilesOfDirs[dirLen];
-    for(i=0;i<dirLen;i++){
+    pthread_t threads[numThreads];//since we want to start reading files and adding to the cyclic buffer
+    for(i=0;i<numThreads;i++){//start numThreads threads
+        pthread_create(&threads[i], 0, threadFunction, 0);
+    }
+
+    int countFilesOfDirs[dirLen];//array usefull for addVaccinationRecords command
+    for(i=0;i<dirLen;i++){//for each country
         DIR *curr_dir;
         if((curr_dir = opendir((pathToDirs[i] + '/').c_str()))== NULL){//open the directory 
             perror("Child: Cant open dir\n");
@@ -118,17 +111,17 @@ int main(int argc, const char** argv) {
             if(strcmp(dirent->d_name, ".") == 0 || strcmp(dirent->d_name, "..")==0){
                 continue;
             }
-            pthread_mutex_lock(&poolMtx);
-            while(currSizePool >= sizePool){
+            pthread_mutex_lock(&poolMtx);//lock the cyclic buffer mutex(since we wont be the only ones need to access this data)
+            while(currSizePool >= cyclicBufferSize){//incase pool is full
                 pthread_cond_wait(&poolFull, &poolMtx);
             }
-            poolEnd = (poolEnd + 1) % sizePool;
+            poolEnd = (poolEnd + 1) % cyclicBufferSize;//when there is empty space inside the pool append new data
             pool[poolEnd] = pathToDirs[i]+'/'+dirent->d_name;
             currSizePool++;
 
 
-            pthread_mutex_unlock(&poolMtx);
-            pthread_cond_signal(&poolEmpty);
+            pthread_mutex_unlock(&poolMtx);//unlock the mutex
+            pthread_cond_signal(&poolEmpty);//and signal the condition variable that there is atleast one data inside the pool
             usleep(0);
             
             count++;
@@ -136,24 +129,24 @@ int main(int argc, const char** argv) {
         countFilesOfDirs[i]=count;
         closedir(curr_dir);
     }
-    for(i=0;i<numThreads;i++){
-        pthread_mutex_lock(&poolMtx);
-        while(currSizePool >= sizePool){
+    for(i=0;i<numThreads;i++){//as before to interact with the cyclic pool we do the same steps, this adds a EXIT protocol message to the buffer
+        pthread_mutex_lock(&poolMtx);//so the threads know that no more data is about to get inserted and exit
+        while(currSizePool >= cyclicBufferSize){
             pthread_cond_wait(&poolFull, &poolMtx);
         }
-        poolEnd = (poolEnd + 1) % sizePool;
+        poolEnd = (poolEnd + 1) % cyclicBufferSize;
         pool[poolEnd] = "EXIT";
         currSizePool++;
         pthread_mutex_unlock(&poolMtx);
         pthread_cond_signal(&poolEmpty);
         usleep(0);
     }
-    for(i=0;i<numThreads;i++){
+    for(i=0;i<numThreads;i++){//and wait for them to close to be able to continue with no sync issues
         pthread_join(threads[i], 0);
     }
 
 
-    pthread_mutex_lock(&mainListMtx);
+    pthread_mutex_lock(&mainListMtx);//a bit unessasary but already setup
     string* temp_blooms = main_list->getBlooms();//after everything is done encode all the bloom filters in a string array to be easier to send to the parent
     writeSocketInt(socke, socketBufferSize, main_list->getCountViruses());//and start sending
     for(i=0;i<main_list->getCountViruses();i++){
@@ -166,6 +159,7 @@ int main(int argc, const char** argv) {
         writeSocket(socke, socketBufferSize, temp_blooms[i]);
     }
     pthread_mutex_unlock(&mainListMtx);
+
     writeSocketInt(socke, socketBufferSize, 0);//finally inform the parent that everything is ok and ready to receive commands and signals
 
 
@@ -173,13 +167,12 @@ int main(int argc, const char** argv) {
     int acceptedRequests=0;
     int rejectedRequests=0;
     while(true){
-        int currFunc= readSocketInt(socke, socketBufferSize);
-        if(currFunc == 106){
-            sizePool = cyclicBufferSize;
-            currSizePool =0;
+        int currFunc= readSocketInt(socke, socketBufferSize);//wait for parent to send a protocol message
+        if(currFunc == 106){//incase we have a 106 command, addVaccinationRecords
+            currSizePool =0;//reset global variables
             poolStart =0;
             poolEnd = -1;
-            for(i=0;i<numThreads;i++){
+            for(i=0;i<numThreads;i++){//open new threads
                 pthread_create(&threads[i], 0, threadFunction, 0);
             }
             for(i=0;i<dirLen;i++){
@@ -199,14 +192,12 @@ int main(int argc, const char** argv) {
                                             //it means that there is more files inside this directory so read them and insert them to the main_list
                     countFilesOfDirs[i]++;
                     pthread_mutex_lock(&poolMtx);
-                    while(currSizePool >= sizePool){
+                    while(currSizePool >= cyclicBufferSize){
                         pthread_cond_wait(&poolFull, &poolMtx);
                     }
-                    poolEnd = (poolEnd + 1) % sizePool;
+                    poolEnd = (poolEnd + 1) % cyclicBufferSize;
                     pool[poolEnd] = pathToDirs[i]+'/'+getCountry(pathToDirs[i])+'-'+to_string(countFilesOfDirs[i]).c_str()+".txt";
-
                     currSizePool++;
-
 
                     pthread_mutex_unlock(&poolMtx);
                     pthread_cond_signal(&poolEmpty);
@@ -216,10 +207,10 @@ int main(int argc, const char** argv) {
             }
             for(i=0;i<numThreads;i++){
                 pthread_mutex_lock(&poolMtx);
-                while(currSizePool >= sizePool){
+                while(currSizePool >= cyclicBufferSize){
                     pthread_cond_wait(&poolFull, &poolMtx);
                 }
-                poolEnd = (poolEnd + 1) % sizePool;
+                poolEnd = (poolEnd + 1) % cyclicBufferSize;
                 pool[poolEnd] = "EXIT";
                 currSizePool++;
                 pthread_mutex_unlock(&poolMtx);
@@ -252,7 +243,7 @@ int main(int argc, const char** argv) {
             }else if(t == 0){
                 rejectedRequests++;
                 totalRequests++;
-            }else if(t == -2){
+            }else if(t == -2){//case exit
                 break;
             }
         }
@@ -266,32 +257,32 @@ int main(int argc, const char** argv) {
     pthread_cond_destroy(&poolEmpty);
     pthread_cond_destroy(&poolFull);
     pthread_mutex_destroy(&poolMtx);
+    pthread_mutex_destroy(&mainListMtx);
     close(socke);
     return 0;
 }
 
 void* threadFunction(void* ptr){
-    while(true){
+    while(true){//infinite while loop since we are stoping when we read specific message from the buffer
         string curr;
-        pthread_mutex_lock(&poolMtx);
+        pthread_mutex_lock(&poolMtx);//lock the mutex
         while(currSizePool <= 0){
-            pthread_cond_wait(&poolEmpty, &poolMtx);
+            pthread_cond_wait(&poolEmpty, &poolMtx);//incase buffer is empty
         }
-        curr = pool[poolStart];
-        poolStart = (poolStart + 1)  % sizePool;
+        curr = pool[poolStart];//whene there is atleast one item
+        poolStart = (poolStart + 1)  % cyclicBufferSize;//read data and update variables
         currSizePool--;
-        pthread_mutex_unlock(&poolMtx);
+        pthread_mutex_unlock(&poolMtx);//unlock mutex
 
-        pthread_cond_signal(&poolFull);
+        pthread_cond_signal(&poolFull);//and signal condition variable to signal that the buffer has atleast one empty space
         usleep(0);
 
-        if(curr == "EXIT"){
+        if(curr == "EXIT"){//check if we received a EXIT protocol message
             break;
         }
 
-        pthread_mutex_lock(&mainListMtx);
-        
-        ifstream records(curr);
+        pthread_mutex_lock(&mainListMtx);//lock main_list mutex
+        ifstream records(curr);//open readed file and insert its lines into the structure
         if(records.fail()){
             perror("Thread: file open ERROR\n");
         }
@@ -301,8 +292,11 @@ void* threadFunction(void* ptr){
             main_list->insertRecord(line, false);
         }
         records.close();
-
-        pthread_mutex_unlock(&mainListMtx);
+        pthread_mutex_unlock(&mainListMtx);//unlock the mutex 
     }
     pthread_exit(0);
+}
+
+string getCountry(string t){//function for addVaccinationRecords, receives a complete country path (relative path) and returns only the country
+    return t.substr(t.find_last_of('/')+1);
 }
--- travelMonitorClient.cpp
+++ travelMonitorClient.cpp
@@ -17,7 +17,7 @@ int main(int argc, const char** argv) {
     int numMonitors, socketBufferSize, cyclicsocketBufferSize, sizeOfBloom, numThreads;
     string pathToDirs;
 
-    if(argc != 13){//simple steps to set the command line arguments to parameters
+    if(argc != 13){//simple steps to set the command line arguments to variables
         cout << "Error! Wrong parameters:" << endl;
         cout << "./travelMonitorClient –m numMonitors -b socketsocketBufferSize -c cyclicsocketBufferSize -s sizeOfBloom -i input_dir -t numThreads" << endl;
         return -1;
@@ -53,6 +53,7 @@ int main(int argc, const char** argv) {
         }
         countryList.insert(dirent->d_name);//insert the countrie's name to the list
     }
+    closedir(inputDir);
 
     int activeMonitors;
     if(countryList.count < numMonitors){//in case the count of the countries is less than the monitors
@@ -63,20 +64,20 @@ int main(int argc, const char** argv) {
 
     int monitorPids[activeMonitors];
 
-    char** toGiveArgs[activeMonitors];
     int i=0;
     int temp;
-    string** toGiveDirs = new string*[activeMonitors];
+    string** toGiveDirs = new string*[activeMonitors];//create an 2D array, [i][j], i indicates the index of the monitor and j the index of the country that the monitor will work on
     for(i=0;i<activeMonitors;i++){
         toGiveDirs[i] = new string[int(countryList.count/activeMonitors)+1];
     }
+    char** toGiveArgs[activeMonitors];//2d array to give arguments for execvp
 
-    if(int(countryList.count/activeMonitors) == float(float(countryList.count)/float(activeMonitors))){
+    if(int(countryList.count/activeMonitors) == float(float(countryList.count)/float(activeMonitors))){//to see if the last monitor has an extra country
         temp = 0;
     }else{
         temp = 1;
     }
-    for(i=0;i<activeMonitors;i++){
+    for(i=0;i<activeMonitors;i++){//first 10 possitions have the arguments that we want to pass to the monitors
         int currSize;
         if(i==activeMonitors-1){
             currSize = 10+int(countryList.count/activeMonitors);
@@ -125,9 +126,9 @@ int main(int argc, const char** argv) {
         toGiveArgs[i][9] = new char[curr.length()+1]();
         strcpy(toGiveArgs[i][9], curr.c_str());
 
-        int j=10;
+        int j;
         int k=0;
-        for(j=10;j<currSize;j++){
+        for(j=10;j<currSize;j++){//the rest of the arguments are the paths 
             string t =countryList.popFirst();
             curr = pathToDirs+'/'+t;
             toGiveArgs[i][j] = new char[curr.length()+1]();//store the country to the array
@@ -138,35 +139,35 @@ int main(int argc, const char** argv) {
         toGiveArgs[i][j] = nullptr;
     }
 
-    int sockets[activeMonitors];
-    int socketfds[activeMonitors];
+    int sockets[activeMonitors];//for socket init
+    int socketFds[activeMonitors];//for keeping the fd for the sockets
 
-    struct sockaddr_in address;
+    struct sockaddr_in address;//for socket init
     struct sockaddr* addressptr = (struct sockaddr *)&address;
-    int opt =1;
+    int opt =1;//used in setsockopt to setup with SO_REUSEPORT flag
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = htonl(INADDR_ANY);
 
     
     for(i=0;i<activeMonitors;i++){
-        if((socketfds[i] = socket(AF_INET, SOCK_STREAM, 0)) <0){
+        if((sockets[i] = socket(AF_INET, SOCK_STREAM, 0)) <0){//init the socket
             perror("error");
             exit(1);
         }
-        address.sin_port = htons(atoi(toGiveArgs[i][1]));
+        address.sin_port = htons(atoi(toGiveArgs[i][1]));//set the port as we set it earlier for the specific monitor
 
-        if(setsockopt(socketfds[i], SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){
-            perror("error");
-            exit(1);
-        }
-
-        if(bind(socketfds[i], addressptr, sizeof(address)) < 0){
+        if(setsockopt(sockets[i], SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))){//setup setsockopt to use the nessasary flags
             perror("error");
             exit(1);
         }
 
-        if(listen(socketfds[i], 1) < 0){
+        if(bind(sockets[i], addressptr, sizeof(address)) < 0){
+            perror("error");
+            exit(1);
+        }
+
+        if(listen(sockets[i], 1) < 0){//since we have 1 socket for each monitor listen only needs 1 connection request
             perror("error");
             exit(1);
         }
@@ -178,11 +179,11 @@ int main(int argc, const char** argv) {
             exit(-1);
             break;
         case 0:
-            execvp("./monitorServer", toGiveArgs[i]);//and execlp the new child to the ./monitor process
-            perror("Parent: Execlp ERROR\n");
+            execvp("./monitorServer", toGiveArgs[i]);//execvp to be able to run executable with an array as parameters(since we dont know how many arguments we need to pass)
+            perror("Parent: Execvp ERROR\n");
             break;
         default:
-            if((sockets[i] = accept(socketfds[i], NULL, NULL)) < 0){
+            if((socketFds[i] = accept(sockets[i], NULL, NULL)) < 0){//wait for child to send a socket request and accept it
                 perror("error");
                 exit(1);
             }
@@ -193,18 +194,18 @@ int main(int argc, const char** argv) {
 
     VirlistHeader viruses(sizeOfBloom);//after all of this is done wait for each monitor to receive the bloom filters
     for(i=0;i<activeMonitors;i++){
-        int tempSize= readSocketInt(sockets[i], socketBufferSize);
+        int tempSize= readSocketInt(socketFds[i], socketBufferSize);
         string tempBlooms[tempSize];//1D array to store the encoded bloom filters for each virus from the monitor
         for(int j=0;j<tempSize;j++){
-            int ts = readSocketInt(sockets[i], socketBufferSize);//read the blooms with error checking in case something happen while reading
+            int ts = readSocketInt(socketFds[i], socketBufferSize);//read the blooms with error checking in case something happen while reading
             while(ts == -1){
-                writeSocketInt(sockets[i], socketBufferSize, -1);
-                ts = readSocketInt(sockets[i], socketBufferSize);
+                writeSocketInt(socketFds[i], socketBufferSize, -1);
+                ts = readSocketInt(socketFds[i], socketBufferSize);
             }
-            writeSocketInt(sockets[i], socketBufferSize, 0);
-            tempBlooms[j] = readSocket(sockets[i], ts, socketBufferSize);//and store it into a temporary string array
+            writeSocketInt(socketFds[i], socketBufferSize, 0);
+            tempBlooms[j] = readSocket(socketFds[i], ts, socketBufferSize);//and store it into a temporary string array
         }
-        if(readSocketInt(sockets[i], socketBufferSize) != 0){//read the confirmation message from the monitor
+        if(readSocketInt(socketFds[i], socketBufferSize) != 0){//read the confirmation message from the monitor
             exit(-1);
         }
         for(int j=0;j<tempSize;j++){//for each virus
@@ -231,21 +232,21 @@ int main(int argc, const char** argv) {
         cout << "Waiting for command: " << flush;
         cin >> command;
         if(command == "/exit"){//exit command
-            closedir(inputDir);
             int status;
             pid_t pid;
             for(i=0;i<numMonitors;i++){
-                writeSocketInt(sockets[i], socketBufferSize, 105);
+                writeSocketInt(socketFds[i], socketBufferSize, 105);//send a protocol message instead of sending a signal
                 pid = waitpid(monitorPids[i], &status, 0);
                 cout << "child " << (long)pid << " got exited " << status << endl;
-                close(socketfds[i]);
+                close(sockets[i]);
+                close(socketFds[i]);
             }
             generateLogFileParent(activeMonitors, int(countryList.count/activeMonitors)+1, toGiveDirs, stats.total, stats.accepted, stats.rejected);
             for(i=0;i<activeMonitors;i++){
                 delete[] toGiveDirs[i];
             }
             delete[] toGiveDirs;
-            for(i=0;i<activeMonitors;i++){
+            for(i=0;i<activeMonitors;i++){//same as creation of the toGiveArgs array
                 int currSize;
                 if(i==activeMonitors-1){
                     currSize = 10+int(countryList.count/activeMonitors);
@@ -261,7 +262,7 @@ int main(int argc, const char** argv) {
         }
         cin.get();
         string line;
-        getline(cin, line);//here we need to finish the command and then do the action
+        getline(cin, line);
         string ctemp[8];//convert the readed string to a string array for more simplicity
         i=0;
         string word = "";
@@ -277,7 +278,7 @@ int main(int argc, const char** argv) {
         ctemp[i] = word;
         i++;
         if(command == "/travelRequest"){
-            travelRequest(&stats, &viruses, sockets, socketBufferSize, activeMonitors, int(countryList.count/activeMonitors)+1, toGiveDirs, monitorPids, stoi(ctemp[0]), ctemp[1], ctemp[2], ctemp[3], ctemp[4]);
+            travelRequest(&stats, &viruses, socketFds, socketBufferSize, activeMonitors, int(countryList.count/activeMonitors)+1, toGiveDirs, monitorPids, stoi(ctemp[0]), ctemp[1], ctemp[2], ctemp[3], ctemp[4]);
             cout << "Done!" << endl;
         }else if(command == "/travelStats"){
             if(i==4){//in case we have a country or not
@@ -290,10 +291,10 @@ int main(int argc, const char** argv) {
                 cout << "Error /travelStatus wrong input" << endl;
             }
         }else if(command == "/addVaccinationRecords"){
-            addVaccinationRecords(sockets, socketBufferSize, activeMonitors, int(countryList.count/activeMonitors)+1, toGiveDirs, monitorPids, ctemp[0], &viruses);
+            addVaccinationRecords(socketFds, socketBufferSize, activeMonitors, int(countryList.count/activeMonitors)+1, toGiveDirs, monitorPids, ctemp[0], &viruses);
             cout << "Done!" << endl;
         }else if(command == "/searchVaccinationStatus"){
-            searchVaccinationStatus(sockets, socketBufferSize, activeMonitors, monitorPids, stoi(ctemp[0]));
+            searchVaccinationStatus(socketFds, socketBufferSize, activeMonitors, monitorPids, stoi(ctemp[0]));
             cout << "Done!" << endl;
         }
         cout << endl;
